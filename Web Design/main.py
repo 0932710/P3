4a02f1cb3d996fa2d95d2eaabe12bdbbdbbcd303
Endy Hu <0937452@hr.nl>
2017-04-10 13:08:59 +0200

    Add files via upload

--- Web Design/main.py
+++ Web Design/main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/maps")
+@app.route("/upload")
 def maps():
-  return render_template("maps.html")
+  return render_template("upload.html")
 
 @app.route("/css")
 def css():
