a5571e8aed7f0aae58764cc5b82d0dca713d008e
0937452 <0937452@hr.nl>
2017-04-18 16:03:13 +0200

    new Heatmap Plugin

--- webDesign/static/main.js
+++ webDesign/static/main.js
@@ -1,10 +1,51 @@
-function createMap() {
-    var map = L.map('map');
+window.onload = function () {
+    var testData = {
+        max: 8,
+        data: [{
+            lat: 51.9232176,
+            lng: 4.47922600000004,
+            count: 8
+        }, {
+            lat: 51.947512,
+            lng: 4.548183999999992,
+            count: 8
+        }, {
+            lat: 51.917202,
+            lng: 4.483986,
+            count: 8
+        }]
+    };
+    var baseLayer = L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+            maxZoom: 18
+        }
+    );
+    var cfg = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        "radius": 0.01,
+        "maxOpacity": .8,
+        // scales the radius based on map zoom
+        "scaleRadius": true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries 
+        //   (there will always be a red spot with useLocalExtremas true)
+        "useLocalExtrema": true,
+        // which field name in your data represents the latitude - default "lat"
+        latField: 'lat',
+        // which field name in your data represents the longitude - default "lng"
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'count'
+    };
+    var heatmapLayer = new HeatmapOverlay(cfg);
+    var map = new L.Map('map', {
+        center: new L.LatLng(51.917202, 4.483986),
+        zoom: 13,
+        layers: [baseLayer, heatmapLayer]
+    });
+    heatmapLayer.setData(testData);
     // Add a tile layer to add to our map, in this case it's the 'standard' OpenStreetMap.org tile server
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        maxZoom: 18
-    }).addTo(map);
 
     var politieIcon = L.icon({
         iconUrl: '/static/img/politie.png',
@@ -17,7 +58,6 @@ function createMap() {
         [51.947512, 4.548183999999992],
         [51.917202, 4.483986]
     ]
-    politieHeat = politieList
 
     // For each latlong record, add marker to map
     for (i = 0; i < politieList.length; i++) {
@@ -26,26 +66,6 @@ function createMap() {
             icon: politieIcon
         }).addTo(map)
     }
-
-    for (i = 0; i < politieList.length; i++) {
-        politieHeat[i].push(0.5)
-    }
-
-    var heat = L.heatLayer(politieHeat, {
-        minOpacity: 0.2,
-        radius: 100,
-        blur: 0.1,
-        gradient: {
-            0.5: "#07AED8"
-        }
-    }).addTo(map)
-
     // Don't show the 'Powered by Leaflet' text. Attribution overload
     map.attributionControl.setPrefix('');
-
-    // Geographical point (longitude and latitude)
-    var rotterdam = new L.LatLng(51.917202, 4.483986);
-    map.setView(rotterdam, 13);
 }
-
-createMap();
