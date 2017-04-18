f06c23af4c7715067e5562d358ac07a4164bca8c
Comakip <ComakipTP@gmail.com>
2017-04-18 18:04:34 +0200

    Filled Opendata.db

--- data/convert_straatroof.py
+++ data/convert_straatroof.py
@@ -163,7 +163,7 @@ def csvConvert(roof_csv):
     plaats = "ROTTERDAM"
 
     with open(roof_csv) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', encoding='ISO-8859-1') #, quotechar='|')
+        reader = csv.reader(csvfile, delimiter=',') #, quotechar='|')
         for row in reader:
             case = ""
             case_args = {}
