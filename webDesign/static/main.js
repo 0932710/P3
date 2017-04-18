bf6f48cb288a3e559d913d3270e7a5d066bb78c8
pbazuin <pimorez@gmail.com>
2017-04-18 12:44:28 +0200

    Add function to plant multiple markers and heatmaps, change marker icon

--- webDesign/static/main.js
+++ webDesign/static/main.js
@@ -1,18 +1,23 @@
 function createMap() {
     var map = L.map('map');
-    //Add a tile layer to add to our map, in this case it's the 'standard' OpenStreetMap.org tile server
+    // Add a tile layer to add to our map, in this case it's the 'standard' OpenStreetMap.org tile server
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 18
     }).addTo(map);
 
+    var politieIcon = L.icon({
+        iconUrl: '/static/politie.png',
+        iconSize: [60,60]
+    })
+
     // Create list with latlong coords
-    list = [[51.9232176,4.47922600000004], [51.947512,4.548183999999992], [51.917202, 4.483986]]
+    politieList = [[51.9232176,4.47922600000004], [51.947512,4.548183999999992], [51.917202, 4.483986]]
 
     // For each latlong record, add marker to map
-    for (i = 0; i < list.length; i++) {
-        j = list[i]
-        L.marker(j).addTo(map)
+    for (i = 0; i < politieList.length; i++) {
+        latlng = politieList[i]
+        L.marker(latlng,{icon: politieIcon}).addTo(map)
     }
 
     // Don't show the 'Powered by Leaflet' text. Attribution overload
@@ -22,11 +27,12 @@ function createMap() {
     var rotterdam = new L.LatLng(51.917202, 4.483986);
     map.setView(rotterdam, 13);
 
-    // Create heatmap
-    var heat = L.heatLayer([
-            [51.9174402,4.4828688,0.5],
-            [51.9173807,4.4817315,0.5]
-    ], {radius: 100}).addTo(map)
+    for (i = 0; i < politieList.length; i++) {
+        latlng = politieList[i]
+        L.heatLayer(([latlng]), {radius: 100, blur: 50, gradient: {0.5: 'blue'}}).addTo(map)
+    }
+
+
 }
 
 createMap();
