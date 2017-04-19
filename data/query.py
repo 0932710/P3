da61d8e8229465f4158cbd98d3115bba4a83e233
RensMo <0934908@hr.nl>
2017-04-19 17:52:01 +0200

    Working on plots

--- data/query.py
+++ data/query.py
@@ -1,19 +1,19 @@
 from sqlalchemy import *
 
-def query(s):
-    db = create_engine('sqlite:///Opendata.db')
+def query(s, db):
+    db = create_engine(db)
     conn = db.connect()
     result = conn.execute(s)
     return result
 
 if __name__ == '__main__':
     def selectpolicecoords():
-        result = query("SELECT longitude, latitude FROM police_stations")
+        result = query("SELECT longitude, latitude FROM police_stations", 'sqlite:///Opendata.db')
         for i in result:
             print(i)
 
     def selectroofcoords():
-        result = query("SELECT longitude, latitude FROM Straatroven")
+        result = query("SELECT longitude, latitude FROM Straatroven", 'sqlite:///Opendata.db')
         for i in result:
             print(i)
 
--- webDesign/main.py
+++ webDesign/main.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def plot1():
+    result = [{'date':'2013-01', 'value':53}, {'date':'2013-02', 'value':22}]
+    return result
+
+
+def plot2():
+    pass
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", plot1_json = plot1())
 
 @app.route("/upload")
 def maps():
